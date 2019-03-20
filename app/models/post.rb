27a121876ae76884f4de0911fec2ad2f69699eb1
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-19 21:11:53 -0400

    add solution

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,36 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbaity?
+
+  # BAITS = [
+  #   /Won't Believe/i,
+  #   /Secret/i,
+  #   /Top [0-9]*/i,
+  #   /Guess/i
+  # ]
+
+  def clickbaity?
+    baits = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
+    # baits.none? do |b|
+    #   binding.pry
+    #   b.match(title)
+    # end
+
+    if baits.none? {|b| b.match title }
+      errors.add(:title, "must be clickbait")
+    end
+    # BAITS.each do |b|
+    #   if !self.title.include?(b)
+    #     return errors.add(:title, "invalid title without clickbait")
+    #   end
+    # end
+  end
 end
