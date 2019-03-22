9521dd6d90ec4f6954624cbf1b1f5bb38be99aa3
Mickey <micksheridan.24@gmail.com>
2019-03-22 08:58:38 -0400

    submit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, :length => {:maximum => 250}
+  validates :content, :length => {:minimum => 250}
+  validates :category, :inclusion => {in: ["Fiction", "Non-Fiction"]}
+  validate :click_bait?
+
+  def click_bait?
+    title = self.title
+    if (title != "" && title != nil)
+      if (!(title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess")))
+        errors.add(:title, "needs to be more clickbaity")
+      end
+    end
+  end
 end
